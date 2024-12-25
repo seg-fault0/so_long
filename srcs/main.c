66c736b5a2a251e4455cadbd649cf1f6f281182c
Walid Imam <wimam@e2r13p5.1337.ma>
2024-12-25 16:42:44 +0100

    add a start function

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,17 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 16:27:05 by wimam             #+#    #+#             */
-/*   Updated: 2024/12/25 16:32:03 by wimam            ###   ########.fr       */
+/*   Updated: 2024/12/25 16:41:52 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	ft_start(t_mlx *mlx)
+{
+	ft_map_gen(mlx);
+	ft_mlx_print_img(mlx, mlx->img->player_down, mlx->px, mlx->py);	
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,8 +27,7 @@ int	main(int argc, char *argv[])
 	mlx = ft_mlx_init(argv[1]);
 	if (!mlx)
 		return (-1);
-	ft_map_gen(mlx);
-	ft_mlx_print_img(mlx, mlx->img->player_down, mlx->px, mlx->py);
+	ft_start(mlx);
 	mlx_key_hook(mlx->win, ft_input, mlx);
 	mlx_hook(mlx->win, 17, 0, ft_exit, mlx);
 	mlx_loop(mlx->mlx);
