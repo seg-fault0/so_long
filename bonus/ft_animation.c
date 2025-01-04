ab22f25c5cee24c8126c29af19fef8128fc38e20
Walid Imam <wimam@e1r10p12.1337.ma>
2025-01-04 09:30:27 +0100

    enemy pattern role

--- bonus/ft_animation.c
+++ bonus/ft_animation.c
@@ -6,11 +6,41 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 17:42:24 by wimam             #+#    #+#             */
-/*   Updated: 2025/01/03 13:45:59 by wimam            ###   ########.fr       */
+/*   Updated: 2025/01/04 09:28:16 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
+
+void	ft_fox_mov(t_mlx *mlx)
+{
+	int			i;
+	int			j;
+	static int	dir;
+
+	i = 0;
+	while (mlx->map[i] != NULL)
+	{
+		j = 0;
+		while (mlx->map[i][j] != '\n')
+		{
+			if(mlx->map[i][j] == 'B' && mlx->map[i][j+1] == '0' && dir == 0)
+			{
+				mlx->map[i][j] = '0';
+				mlx->map[i][++j] = 'B';
+				dir = 1;
+			}
+			else if(mlx->map[i][j] == 'B' && mlx->map[i][j-1] == '0' && dir == 1)
+			{
+				mlx->map[i][j] = '0';
+				mlx->map[i][--j] = 'B';
+				dir = 0;
+			}
+			j++;
+		}
+		i++;
+	}	
+}
 
 void	ft_get_frames(t_mlx *mlx)
 {
@@ -37,15 +67,19 @@ void	ft_get_frames(t_mlx *mlx)
 int	ft_animation(t_mlx *mlx)
 {
 	static int	i = 1;
-	int			speed;
+	int			anim_speed;
+	int			fox_mv_speed;
 
-	speed = 5000;
-	if (i % speed == 0)
+	anim_speed = 5000;
+	fox_mv_speed = 50000;
+	if (i % anim_speed == 0)
 	{
 		ft_mov(mlx, 0);
 		ft_get_frames(mlx);
 	}
-	if (i == speed * 100)
+	if (i % fox_mv_speed == 0)
+		ft_fox_mov(mlx);
+	if (i == anim_speed * 1000)
 		i = 1;
 	i++;
 	return (0);
